08b82c1c8cc66901869f2ffd59d0c4446cb40e95
Paul Logston <paul.logston@gmail.com>
2020-02-24 16:37:35 -0500

    Finish app

--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
+import random
+
 from django.views.generic.base import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = "app/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['number'] = random.randint(1, 5)
+        return context
